06502f49a7150e746025b1797bab32606c921dd1
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-06-15 15:00:37 +0530

    sonia

---
+++ login.js
@@ -0,0 +1,41 @@
+let login=document.querySelector("div>#login");
+let datalogin=JSON.parse(localStorage.getItem("logindata"))||[];
+login.style.cursor="pointer";
+login.addEventListener("click", clicklogin);
+let userInfo=JSON.parse(localStorage.getItem("Userdata")) || [];
+function clicklogin(){
+
+let email1=document.querySelector("#email").value;
+    let pass=document.querySelector("#password").value;
+    let x="No";
+    userInfo.map(function(element){
+        if(element.email===email1&&element.password===pass){
+            x="Yes";
+            
+            window.location.href=""
+            
+        } else 
+        {
+            x="No";
+        } 
+        
+    })
+    if(x==="Yes"){
+        alert("successfully login");
+        let obj={email:email1, 
+        password:pass}
+        datalogin.push(obj);
+        localStorage.setItem("logindata",JSON.stringify(datalogin));
+        window.location.href="women.html";
+    }
+    else if(x==="No"){
+        alert("wrong password or email");
+    } 
+    
+}
+let sign=document.querySelector("#signup");
+sign.style.cursor="pointer";
+sign.addEventListener("click",submit);
+function submit (){
+    window.location.href="signUpCW.html";
+}
